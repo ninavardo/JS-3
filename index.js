57128e5d38e1f8ec58fc0132f77b5a61d03927ce
Nina Vardosanidze <nina@Ninas-MacBook-Air.local>
2026-07-15 16:39:46 +0400

    JS-3 homeworkN3

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // davaleba n1
 
-// let n = 0;
+let n = 0;
 
-// for (let i = 1; i <= 20; i++) {
-//   if (i % 2 === 0) {
-//     n += i;
-//   }
-// }
-// console.log(n);
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    n += i;
+  }
+}
+console.log(n);
 
 // -----------------------
 
@@ -23,3 +23,40 @@ if (tech.indexOf(input) === -1) {
 }
 
 // -----------------------
+
+
+// davaleba n3
+
+let movies = [
+  {
+    name: "Interstellar",
+    year: 2014,
+    rating: 8.7,
+  },
+  {
+    name: "Inception",
+    year: 2010,
+    rating: 8.8,
+  },
+  {
+    name: "The Batman",
+    year: 2022,
+    rating: 7.9,
+  },
+];
+
+let count = 0;
+
+for (let i = 0; i < movies.length; i++) {
+
+//   console.log("Movie name: " + movies[i].name );
+
+  if (movies[i].rating > 8.0) {
+    count++;
+
+    console.log(
+      "name: " + movies[i].name + " Movie rating: " + movies[i].rating,
+    );
+  }
+}
+console.log("filmebis raodenoba romelta reitingi > 8.0: " + count);

b41a8ef0e37676dadc1dc528e4ea340b9656246f
Nina Vardosanidze <nina@Ninas-MacBook-Air.local>
2026-07-15 16:12:50 +0400

    JS-3 homeworkN2

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 // davaleba n1
 
-let n = 0;
+// let n = 0;
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 2 === 0) {
-    n += i;
-  }
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     n += i;
+//   }
+// }
+// console.log(n);
+
+// -----------------------
+
+// davaleba n2
+
+let tech = ["Laptop", "Phone", "Keyboard", "Mouse", "Monitor"];
+
+input = prompt("sheiyvane produqtis dasaxeleba:");
+if (tech.indexOf(input) === -1) {
+  alert("produqti ar arsebobs");
+} else {
+  alert("produqti arsebobs misi indexi: " + tech.indexOf(input));
 }
-console.log(n);
 
 // -----------------------
